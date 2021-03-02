a53337238a94894163a26d96668c151eb2724d70
Valentyn Chubukin <val@valch.name>
2021-03-03 00:10:27 +0200

    calsses

---
+++ class.groovy
@@ -0,0 +1,33 @@
+class User{
+    String lastName;
+    String firstName;
+
+    public String UserName(){
+        return getUserName(this.firstName, this.lastName);
+    }
+
+    private String getUserName(String firstName, String lastName) {
+        return firstName.substring(0,1).toLowerCase() + "_" + lastName.toLowerCase();
+    }
+}
+
+/*
+String[] firstNames = ["Bob", "Jeff", "Roy", "George", "Tom"]
+String[] lastNames = ["Dylan", "Lynne", "Orbison", "Harrison", "Petty"]
+
+for(int i = 0; i < firstNames.size(); i++){
+    User u = new User(firstName: firstNames[i], lastName: lastNames[i]);
+    println("UserName is ${u.UserName()}");
+}
+
+*/
+
+User[] users = [new User(firstName: "Bob", lastName: "Dylan"),
+                new User(firstName: "Jeff", lastName: "Lynne"),
+                new User(firstName: "Roy", lastName: "Orbison"),
+                new User(firstName: "George", lastName: "Harrison"),
+                new User(firstName: "Tom", lastName: "Petty")];
+
+//users.each(user -> println("UserName is ${user.UserName()}"));
+users.each(user -> println(user));
+
