04f13d365e0bb2f87f6b1a760be0919853f7fb62
Valentyn Chubukin <val@valch.name>
2021-03-02 18:53:39 +0200

    add comments

--- controlstructures.groovy
+++ controlstructures.groovy
@@ -2,20 +2,28 @@ int courseCount = 14;
 Boolean isProgrammer = true;
 String[] singers = ["Bob", "George", "Jeff", "Roy", "Tom"]
 
-/*if (isProgrammer) {
+//One line comment 
+
+/*
+Several 
+lines
+comment
+*/
+
+if (isProgrammer) {
     println "He's a programmer, alright"
 }
 else {
     println "Not a programmer, tho"
-}*/
+}
 
-//for (int i =0; i < courseCount; i++) {
-//    println "Chris made course " + (i+1) + "!!!"
-//}
+for (int i =0; i < courseCount; i++) {
+    println "Chris made course " + (i+1) + "!!!"
+}
 
-//for (String singer: singers){
-//    println singer
-//}
+for (String singer: singers){
+    println singer
+}
 
 singers.each{x -> println(x)}
 singers.each{println(it)}
