0a2ac43af3409fda11495db6ae4a1826d97603e2
Valentyn Chubukin <val@valch.name>
2021-03-02 13:46:33 +0200

    changes

---
+++ data_types.groovy
@@ -0,0 +1,10 @@
+String name = "Valentin"
+int courseCount = 14
+float salary = 99999999.99
+BigDecimal salaryB = 99999999.99
+Boolean isProgrammer = true
+
+println name + " hase created " + courseCount + " courses."
+println name + " is programmaer? " + isProgrammer
+println name + " wishes his salary was " + salary
+println name + " wishes his salary was " + salaryB
