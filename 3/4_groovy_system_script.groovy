05926bb0096a0d2e18ff243dfc7af65394fbec48
Valentyn Chubukin <val@valch.name>
2021-03-11 16:15:48 +0200

    some changes

--- 3/4_groovy_system_script.groovy
+++ 3/4_groovy_system_script.groovy
@@ -1,5 +1,5 @@
 import jenkins.model.Jenkins;
-def target = binding.variables["target"]
+def target = build.buildVariableResolver.resolve("target")
 def svr = Jenkins.instance;
 def job = svr.getJob(target);
 def sched = job.scheduleBuild2(0);
