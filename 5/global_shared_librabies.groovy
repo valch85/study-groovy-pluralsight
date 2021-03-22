01f103900c2dfbd4ba5a1827b105d1ea3efc3ea0
Valentyn Chubukin <val@valch.name>
2021-03-22 17:40:37 +0200

    folders without size

--- 5/global_shared_librabies.groovy
+++ 5/global_shared_librabies.groovy
@@ -3,10 +3,16 @@ import groovy.io.*;
 
 def dir = new File("../ConsoleApp"); //pwd();
 
-new File(dir.path + '/releasenotes.txt').withWriter('utf-8')
-{
+new File(dir.path + '/releasenotes.txt').withWriter('utf-8'){
     writer -> 
-        dir.eachFileRecurse(FileType.ANY){file -> writer.writeLine(file.name + '\t' + file.length());}
+        dir.eachFileRecurse(FileType.ANY){file ->
+            if (file.isDirectory()){
+                writer.writeLine(file.name);
+            }
+            else{
+                writer.writeLine(file.name + '\t' + file.length());
+            }
+    }
 }
 
 
